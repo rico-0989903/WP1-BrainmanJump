a33fb8d8d457da5f6ea7bb10a45ddf71abf7b25f
rico-0989903 <0989903@hr.nl>
2022-09-28 14:11:44 +0200

    feat: base template player functions & variables.

--- game.py
+++ game.py
@@ -8,19 +8,32 @@ SCREEN_WIDTH = 500
 SCREEN_HEIGT = 800
 clock = pygame.time.Clock()
 
+# Player variables
+SPEED = 10
+STARTING_POSITION = 120
+
 # screen setup
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGT))
 pygame.display.set_caption('BrainJump')
 
 # classes
-class Player():
+class Player(object):
 
-    def __init__(self):
-        #TODO set position
-        self.position = 0
+    def __init__(self, position):
+        #TODO set base position
+        self.position = position
+
+    def MoveRight(self):
+        #TODO move position to right
+        self.postion += SPEED
+
+    def MoveLeft(self):
+        #TODO move position to left
+        self.position -= SPEED
 
 # game loop
 run = True
+player = Player(STARTING_POSITION)
 while run:
 
     # events
