12b061cb74141235d2c585a6512419689a9b8bbe
Roman-1051628 <1051628@hr.nl>
2022-09-28 12:10:47 +0200

    Base game made

--- game.py
+++ game.py
@@ -1,1 +1,23 @@
 import pygame
+
+pygame.init()
+
+# variables
+FPS = 60
+SCREEN_WIDTH = 500
+SCREEN_HEIGT = 800
+
+# screen setup
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGT))
+pygame.display.set_caption('BrainJump')
+
+# game loop
+run = True
+while run:
+
+    # events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
