2e36f01a4fd3bb6d917ddd6d921506835b1c2fb8
Younes-1006936 <1006936@hr.nl>
2022-10-11 17:12:28 +0200

    mouse click

--- mainscreen.py
+++ mainscreen.py
@@ -24,18 +24,38 @@ class Button():
         #images Height and width info
         width = image.get_width()
         height = image.get_width()
+
         # Image size
         #self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale))
+
        # Images rectangle 
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
+        self.clicked = False
 # Buttoon on screen
     def draw(self):
+
+        #cursor position on screen
+        pos = pygame.mouse.get_pos()
+
+        #Cursor coordinates
+        #print(pos)
+
+        #Mouse click Leftbutton:[0]
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+        
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+
+        #draw buttons on screen
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
-start_button = Button(100, 200, start_image)
-exit_button = Button(450, 200, exit_img)
+start_button = Button(100, 200, start_image, #OG * size)
+exit_button = Button(450, 200, exit_img, #OG * size)
 
 
 
