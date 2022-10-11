b2d990eb210b74adcebf8263b8736c27479066d5
Younes-1006936 <1006936@hr.nl>
2022-10-11 16:30:19 +0200

    mainscreen temp

---
+++ mainscreen.py
@@ -0,0 +1,27 @@
+import pygame
+
+pygame.init()
+
+# variables
+FPS = 60
+SCREEN_WIDTH = 500
+SCREEN_HEIGT = 800
+clock = pygame.time.Clock()
+MAX_PLATFORMS = 10
+
+# screen setup
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGT))
+pygame.display.set_caption('BrainJump')
+
+
+start_img = pygame.image.load('start_btn.png').convert_alpha()
+
+run = True 
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+    pygame.display.update()
+
+pygame.quit()
